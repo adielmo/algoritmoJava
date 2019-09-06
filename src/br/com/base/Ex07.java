a4028325d85b285415ea71067bc0b3fbdfafecaa
adielmo <adielmorabelo@gmail.com>
2019-09-06 18:06:24 -0300

    Nova features do Ex07
    Please enter the commit message for your changes. Lines starting

---
+++ src/br/com/base/Ex07.java
@@ -0,0 +1,42 @@
+package br.com.base;
+
+import java.util.Stack;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		
+		imprimir(2);
+		imprimir(4);
+		imprimir(10);
+		imprimir(25);
+		imprimir(10035);
+		
+		
+	}
+	
+	public static void imprimir(int numero) {
+		System.out.println(numero + " ess número em binário: "
+				+ decimalBinario(numero));
+	}
+	
+	public static String decimalBinario(int numero) {
+		Stack<Integer> pilha = new Stack<>();
+		String numBinario ="";
+		int resto;
+		
+		while(numero > 0) {
+			resto = numero % 2;
+			pilha.push(resto);
+			
+			numero /= 2;
+			
+		}
+		while(!pilha.isEmpty()) {
+			numBinario += pilha.pop();
+		}
+		
+		return numBinario;
+	}
+
+}
+
